873b406a09095e39aeeeb493c911e5951e9267e6
Jonathan Ruiz <jonathan.ruiz@rocket-internet.de>
2014-08-21 11:34:02 +0100

    Switching to csv file as output

--- src/main/java/es/jonathanruiz/owl/GetOwlDataExample.java
+++ src/main/java/es/jonathanruiz/owl/GetOwlDataExample.java
@@ -7,6 +7,9 @@ import org.semanticweb.owlapi.util.OWLOntologyWalker;
 import org.semanticweb.owlapi.util.OWLOntologyWalkerVisitor;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.Set;
@@ -18,10 +21,9 @@ import java.util.Set;
 public class GetOwlDataExample {
     public OWLOntologyManager manager;
 
-    public void showClasses() throws OWLOntologyCreationException {
+    public void showClasses() throws OWLOntologyCreationException, FileNotFoundException, UnsupportedEncodingException {
         manager = OWLManager.createOWLOntologyManager();
         OWLDataFactory factory = manager.getOWLDataFactory();
-
 
         File file = new File("/tmp/hp.owl");
         OWLOntology hpOntology = manager.loadOntologyFromOntologyDocument(file);
@@ -29,23 +31,39 @@ public class GetOwlDataExample {
         Set<OWLClass> currentOntologyClasses = hpOntology.getClassesInSignature();
         System.out.println("Classes in Ontology: " + currentOntologyClasses.size());
 
+        PrintWriter writer = new PrintWriter("/tmp/hp.csv", "UTF-8");
+
         for (Iterator<OWLClass> it = currentOntologyClasses.iterator(); it.hasNext(); ) {
             OWLClass owlClass = it.next();
-            System.out.println(owlClass.toStringID());
-            System.out.println("===============================");
             Set<OWLAnnotation> annotations = owlClass.getAnnotations(hpOntology);
+
+            String id = "";
+            String iao115 = "";
 
             for (Iterator<OWLAnnotation> itAnnotations = annotations.iterator(); itAnnotations.hasNext(); ) {
                 OWLAnnotation oWLAnnotation = itAnnotations.next();
-                //System.out.println("\tAnnotation: " + oWLAnnotation.toString());
-                //System.out.println("\tAnnotation value: " + oWLAnnotation.getValue());
                 String iriFragment = oWLAnnotation.getProperty().getIRI().getFragment();
+
                 if (oWLAnnotation.getValue() instanceof OWLLiteral && (iriFragment.equals("id") || iriFragment.equals("IAO_0000115"))) {
                     OWLLiteral val = (OWLLiteral) oWLAnnotation.getValue();
-                    System.out.println("\t" + iriFragment + ":" + val.getLiteral());
+                    if(iriFragment.equals("id")) {
+
+                        id = val.getLiteral();
+                    }
+                    if(iriFragment.equals("IAO_0000115")) {
+
+                        iao115 = val.getLiteral();
+                    }
                 }
             }
+            if (!id.isEmpty() && !iao115.isEmpty()) {
+                writer.println("\"" + id + "\"" + "," + "\"" + iao115 + "\"" );
+            }
+
+
+
         }
+        writer.close();
         manager.removeOntology(hpOntology);
     }
 
@@ -59,6 +77,10 @@ public class GetOwlDataExample {
             ex.showClasses();
         } catch (OWLOntologyCreationException e) {
             System.out.println("[Error] Exception throw:");
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
